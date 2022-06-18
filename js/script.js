b1b2e0a4cb21b3e34671de3f9d0999a241a7e247
Mdaiimeur <nc94.etude@gmail.com>
2022-06-18 02:33:17 +0200

    js1

---
+++ js/script.js
@@ -0,0 +1,105 @@
+// On charge les informations utiles
+const statut = document.querySelector("h2")
+let jeuActif = true
+let joueurActif = "X"
+let etatJeu = ["", "", "", "", "", "", "", "", ""]
+
+// On définit les conditions de victoire
+const conditionsVictoire = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+// Messages
+const gagne = () => `Player ${joueurActif} has won`
+const egalite = () => "Draw"
+const tourJoueur = () => `Player ${joueurActif} you're up`
+
+// On affiche quel joueur commence
+statut.innerHTML = tourJoueur()
+
+// On met en place les écouteurs d'évènements
+document.querySelectorAll(".case").forEach(cell => cell.addEventListener("click", gestionClicCase))
+document.querySelector("#retry").addEventListener("click", retry)
+
+/**
+ * Cette fonction gère le clic sur les cases du jeu
+ */
+function gestionClicCase(){
+    // On récupère l'index de la case cliquée
+    const indexCase = parseInt(this.dataset.index)
+    
+    // On vérifie si la case est déjà remplie ou le jeu terminé
+    if(etatJeu[indexCase] !== "" || !jeuActif){
+        return
+    }
+
+    // On écrit le symbole du joueur dans le tableau etatJeu et la case
+    etatJeu[indexCase] = joueurActif
+    this.innerHTML = joueurActif
+
+    // On vérifie si le joueur a gagné
+    verifGagne()
+}
+
+/**
+ * Cette fonction vérifie si le joueur a gagné
+ */
+function verifGagne(){
+    let tourGagnant = false
+
+    // On parcourt toutes les conditions de victoire
+    for(let conditionVictoire of conditionsVictoire){
+        // On récupère les 3 cases de la condition de victoire
+        let val1 = etatJeu[conditionVictoire[0]]
+        let val2 = etatJeu[conditionVictoire[1]]
+        let val3 = etatJeu[conditionVictoire[2]]
+
+        // Si l'une des cases est vide
+        if(val1 === "" || val2 === "" || val3 === ""){
+            continue
+        }
+
+        // Si les 3 cases sont identiques
+        if(val1 === val2 && val2 === val3){
+            // On gagne
+            tourGagnant = true
+            break
+        }
+    }
+
+    // Si on a gagné
+    if(tourGagnant){
+        statut.innerHTML = gagne()
+        jeuActif = false
+        return
+    }
+
+    // Si toutes les cases sont remplies
+    if(!etatJeu.includes("")){
+        statut.innerHTML = egalite()
+        jeuActif = false
+        return
+    }
+
+    // On change de joueur
+    joueurActif = joueurActif === "X" ? "O" : "X"
+    statut.innerHTML = tourJoueur()
+}
+
+/**
+ * Cette fonction réinitialise le jeu
+ */
+function retry(){
+    joueurActif = "X"
+    jeuActif = true
+    etatJeu = ["", "", "", "", "", "", "", "", ""]
+    statut.innerHTML = tourJoueur()
+    document.querySelectorAll(".case").forEach(cell => cell.innerHTML = "")
+}
